4864895b41e7ddd01d7955c5bac26b2af3f84d1f
DJIKINE22 <djikinemamadou2@gmail.com>
2023-10-06 17:38:50 +0000

    commit produitclass

--- src/main/java/com/G5GestionProduit/application/Produit.java
+++ src/main/java/com/G5GestionProduit/application/Produit.java
@@ -1,5 +1,34 @@
 package com.G5GestionProduit.application;
 
 public class Produit {
+	private int id;
+	private String nom;
+	private String caracterisque;
+	private int prix;
+	private int quantite;
+	
+	public Produit() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Produit(String nom, String caracterisque, int prix, int quantite) {
+		super();
+		this.nom = nom;
+		this.caracterisque = caracterisque;
+		this.prix = prix;
+		this.quantite = quantite;
+	}
+
+	public Produit(int id, String nom, String caracterisque, int prix, int quantite) {
+		super();
+		this.id = id;
+		this.nom = nom;
+		this.caracterisque = caracterisque;
+		this.prix = prix;
+		this.quantite = quantite;
+	}
+	
+	
 
 }
